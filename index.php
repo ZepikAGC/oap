9027d488dd2a9c04a9f620720c83ae797d6ea119
mrzep <ponybro734@gmail.com>
2020-04-23 20:33:28 +0300

    fourth laba

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+use Osipov\Log;
+use Osipov\QuadraticSolve;
+use Osipov\MyException;
+
+include 'core/SolveInterface.php';
+include 'core/LogInterface.php';
+include 'core/LogAbstract.php';
+include 'Osipov/MyException.php';
+include 'Osipov/LinearSolve.php';
+include 'Osipov/QuadraticSolve.php';
+include 'Osipov/Log.php';
+
+$eq=new QuadraticSolve();
+
+$a=0;
+$b=0;
+$c=0;
+
+try {
+    function checkdata($num,$letter)
+    {
+        $pattern = '#^[0-9]*[.]?[0-9]+$#';
+        for (;;) {
+            $num=readline("Enter $letter=");
+            echo "\n";
+            if(preg_match($pattern,$num))
+            {
+                return $num;
+            }
+            else {
+                echo "Inappropriate symbols. Can only type numbers and dot\n";
+            }
+        }
+        return $num;
+    }
+
+    $a=checkdata($a,'a');
+    $b=checkdata($b,'b');
+    $c=checkdata($c,'c');
+
+    $eq->solve($a,$b,$c);
+} catch (MyException $e) {
+    Log::log("Error: ".$e->getMessage());
+}
+
+Log::write();

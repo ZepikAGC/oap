26c131afaf08172d9998854a2d8a277715030892
mrzep <ponybro734@gmail.com>
2020-04-23 20:30:06 +0300

    second laba

--- index.php
+++ index.php
@@ -1,23 +1,38 @@
 <?php
-	class B 
-	{
-	}
-	class A extends B
-	{
-		public $a1;
-        public $a2;
 
-		function __construct($a,$b)
-		{
-			$this->a1 = $a;
-			$this->a2 = $b;
-		}
-	}
+class MyException extends RuntimeException { }
+class A {
+    protected $x;
+    function solve($a,$b)
+    {
+        try {
+            if ($a==0) throw new MyException("Equation does not exist", 1);
+            else $x=(-1*$b)/$a;
+        } catch (MyException $e) $x=$e;
+        $this->x=$x;
+        return $x;
+    }
+}
 
-	$b1=new B();
-	$b2=new B();
-	$b3=new B();
+class B extends A {
+    protected function discr($a,$b,$c) {
+        $discr=pow($b,2)-4*$a*$c;
+        try if ($discr<0) throw new MyException("Discriminant less than zero", 1);
+        catch (MyException $e) $discr=$e; 
+        return $discr;
+    }
 
-	$a2=new A($b2,$b3);
-	$a1=new A($b1,$a2);
-?>
+    function solve($a,$b,$c)
+    {
+        $discr=$this->discr($a,$b,$c);
+        if (is_object($discr)==true) return $discr;
+        else if ($a==0) $x[]=parent::solve($b,$c);
+        else if ($discr == 0) $x[]=($b*-1)/(2*$a);        }
+        else {
+            $x[]=(($b*-1)+sqrt($discr))/(2*$a);
+            $x[]=(($b*-1)-sqrt($discr))/(2*$a);
+        }
+        $this->x=$x;
+        return $x;
+    }
+}
